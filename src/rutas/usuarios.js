b9b36cf6887073976d8521e69edc5482a514f525
eliana539 <elianacortez27@gmail.com>
2022-07-30 21:17:20 -0300

    infromacion de cada alumno

--- src/rutas/usuarios.js
+++ src/rutas/usuarios.js
@@ -26,13 +26,23 @@ ruta.get('/principal', async (req, res)=> {
 }); */
 
  ruta.get('/listadousuario', async (req, res)=> {
-    const usuarios= await db.query ('select * from usuarios');
-    console.log(usuarios)
+    const usuarios = await db.query ('select legajo, nombre, apellido, dni from usuarios');
     res.render('paginas/listadoUsuario', {usuarios});
 }); 
 /* ruta.post('/listadoUsuario', async (req, res)=> {
     res.redirect('recibido');
 }); */
+ ruta.get('/informacionalumno/:legajo', async (req, res)=>{ 
+    const{legajo} = req.params;
+    const alumno = await db.query ('select usuarios.legajo, nombre, apellido, dni, domicilio, fechaNacimiento from usuarios where usuarios.legajo =?', [legajo]);
+    const alumnoTel = await db.query ('select telefono from usuarios_tel where usuarios_tel.legajo =?',[legajo]);
+    const alumnoEmail = await db.query ('select email from usuarios_mail where usuarios_mail.legajo =?', [legajo]);
+    console.log(alumno)
+    console.log(alumnoEmail)
+    console.log(alumnoTel)
+    res.render('paginas/informacionAlumno', {alumno,alumnoTel,alumnoEmail});
+ });
+
 
 
 module.exports=ruta;
